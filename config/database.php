783a1c0b52b1af244f729aea0c0e41934d82f8e8
mahfoud <mail@esi.com>
2018-03-08 00:45:12 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -1,17 +1,19 @@
 <?php
-[
-    'mysql' => [
-        'driver' => 'mysql',
-        'host' => env('DB_HOST', 'db4free.net'),
-        'port' => env('DB_PORT', '3306'),
-        'database' => env('DB_DATABASE', 'tharwa_db'),
-        'username' => env('DB_USERNAME', 'tharwa_admin'),
-        'password' => env('DB_PASSWORD', 'THARWAtharwa02@@'),
-        'unix_socket' => env('DB_SOCKET', ''),
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_unicode_ci',
-        'prefix' => '',
-        'strict' => true,
-        'engine' => null,
-    ],
+return [
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'tharwa_db'),
+            'username' => env('DB_USERNAME', 'tharwa_admin'),
+            'password' => env('DB_PASSWORD', 'THARWAtharwa02@@'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+    ]
 ];
