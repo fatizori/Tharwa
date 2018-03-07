93db0689ed5c81ac07222524751fc1b876089146
mahfoud <mail@esi.com>
2018-03-08 00:51:40 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,17 @@
 <?php
-return [
+    return [
+        /*
+        |--------------------------------------------------------------------------
+        | Default Database Connection Name
+        |--------------------------------------------------------------------------
+        |
+        | Here you may specify which of the database connections below you wish
+        | to use as your default connection for all database work. Of course
+        | you may use many connections at once using the Database library.
+        |
+        */
+        'default' => env('DB_CONNECTION', 'mysql'),
+
     'connections' => [
         'mysql' => [
             'driver' => 'mysql',
