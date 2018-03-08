4c7544c71168b1d80f7def39b33320dcd570484a
mahfoud <mail@esi.com>
2018-03-08 12:44:54 +0100

    database

--- config/database.php
+++ config/database.php
@@ -11,21 +11,22 @@
         |
         */
         'default' => env('DB_CONNECTION', 'mysql'),
+        'migrations' => 'migrations',
 
-    'connections' => [
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'tharwa_db'),
-            'username' => env('DB_USERNAME', 'tharwa_admin'),
-            'password' => env('DB_PASSWORD', 'THARWAtharwa02@@'),
-            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-    ]
+        'connections' => [
+            'mysql' => [
+                'driver' => 'mysql',
+                'host' => env('DB_HOST', 'db4free.net'),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'tharwa_db'),
+                'username' => env('DB_USERNAME', 'tharwa_admin'),
+                'password' => env('DB_PASSWORD', 'THARWAtharwa02@@'),
+                'unix_socket' => env('DB_SOCKET', ''),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
+        ]
 ];
