f4ffdced637323be6d4adb090e4578caeb391913
mahfoud <mail@esi.com>
2018-03-09 18:20:49 +0100

    no force ssl

--- routes/auth.php
+++ routes/auth.php
@@ -3,14 +3,12 @@
 
 
 //route to authenticate (1st step send credentials)
-$app->post('login', ['uses' => 'LoginsController@sendCodeLogin',
-    'middleware' => ['force_ssl']]);
+$app->post('login', ['uses' => 'LoginsController@sendCodeLogin']);
 
 //$app->post('login', ['uses' => 'LoginsController@sendCodeLogin']);
 
 //route to authenticate (2nd step send code)
-$app->post('login/code', ['uses' => 'LoginsController@Login',
-    'middleware' => ['force_ssl']]);
+$app->post('login/code', ['uses' => 'LoginsController@Login']);
 
 //route to logout
 $app->post('logout', ['middleware' => 'auth',
