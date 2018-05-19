319a1d8e2c6f8ca84f182790f641e56095dc5a78
Fatizo <ef_mezerreg@esi.dz>
2018-05-19 15:37:21 +0100

    Virement Externe:modification

--- routes/auth.php
+++ routes/auth.php
@@ -6,10 +6,10 @@
 //$app->post('login', ['uses' => 'LoginsController@sendCodeLogin',
 //    'middleware' => ['force_ssl']]);
 
-//$app->post('login', ['uses' => 'LoginsController@askCodeLogin']);
+$app->post('login', ['uses' => 'LoginsController@askCodeLogin']);
 
 //route to authenticate (2nd step send code)
-$app->post('login', ['uses' => 'LoginsController@Login']);
+$app->post('login/code', ['uses' => 'LoginsController@Login']);
 
 //route to logout
 $app->post('logout', ['middleware' => 'auth',
